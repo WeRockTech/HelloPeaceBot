d01d990252dcc842493792a48b88152098798d48
Eddie Jaoude <eddie@jaoudestudios.com>
2017-09-23 15:07:36 +0100

    Fixed twitter comms

--- handler.js
+++ handler.js
@@ -1,9 +1,13 @@
 'use strict';
-const TwitterPackage = require('twitter');
-const config = require('./configs/config');
+const TwitterClient = require('twitter');
 
 module.exports.hello = (event, context, callback) => {
-  const Twitter = new TwitterPackage(config);
+  const Twitter = new TwitterClient({
+    "consumer_key": process.env.CONSUMER_KEY,
+    "consumer_secret": process.env.CONSUMER_SECRET,
+    "access_token_key": process.env.TOKEN_KEY,
+    "access_token_secret": process.env.TOKEN_SECRET
+  });
 
   Twitter.post('statuses/update', { status: 'Hello world \\#PeaceHack \\#PeaceDay' }, function (error, tweet, response) {
     if (error) {
