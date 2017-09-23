9074cf0429c5f8decc41837930d8f2fdff31c140
Tanya Powell <hello@tanyapowell.co.uk>
2017-09-23 14:11:26 +0100

    typo fix

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 'use strict';
-var Twitter = require('./configs/configs.js');
+var Twitter = require('./configs/config.js');
 
 module.exports.hello = (event, context, callback) => {
   Twitter.post('statuses/update', { status: 'Hello world \\#PeaceHack \\#PeaceDay' }, function (error, tweet, response) {
